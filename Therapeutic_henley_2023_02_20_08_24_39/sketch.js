78379cd574e18edd5319b34fb812763f6a9ddcfa
Bilboef <cd.antonius@gmail.com>
2023-02-20 09:27:53 +0100

    hey

---
+++ Therapeutic_henley_2023_02_20_08_24_39/sketch.js
@@ -0,0 +1,22 @@
+function draw() {
+  background(220);
+    image(img, 0, 0);
+}
+let img;
+function preload() {
+  img = loadImage('Nokia.png');
+}
+
+let button;
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  background(0);
+  button = createButton('Restaffald');
+  button.position(width/2, height/2);
+  button.mousePressed(changeBG);
+}
+
+function changeBG() {
+  let val = random(255);
+  background(val);
+}
