ac8e6025a4d05ac1c04ca8f1235b2d41b5bf39a0
Bilboef <cd.antonius@gmail.com>
2023-02-20 13:17:03 +0100

    Update sketch.js

--- Therapeutic_henley_2023_02_20_08_24_39/sketch.js
+++ Therapeutic_henley_2023_02_20_08_24_39/sketch.js
@@ -10,10 +10,13 @@ function draw() {
   image(img2, windowWidth/2, windowHeight/2);
   fill(0);
   rectMode(CENTER);
-      rect(windowWidth/2, windowHeight/5,200,200);
-    image(img, windowWidth/2, windowHeight/5);
+      rect(windowWidth/11, windowHeight/11,200,200);
+      
+    image(img, windowWidth/11, windowHeight/11);
     imageMode(CENTER);
+    img3.resize(windowWidth/1.75, windowHeight/1.75);
     image(img3, windowWidth/2, windowHeight/2);
+    rect(windowWidth/3.245, windowHeight/3,windowWidth/3.245*0.625,windowHeight/3*1.1);
   }
   if(visbillede2){
     
@@ -54,6 +57,6 @@ function changeBG2() {
 
 
 function mousePressed(){
-print(mouseX,mouseY);
-
+  if(mouseX>windowWidth/2.92 && mouseX<windowWidth/2.92*0.47 && mouseY>windowHeight/4.725 && mouseY<4.725*1.375)
+print('MAD');
 }
