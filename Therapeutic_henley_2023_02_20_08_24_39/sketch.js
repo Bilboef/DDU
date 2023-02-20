39ed72e1c85ed3a6f397f507f738f15aa70a671d
Bilboef <cd.antonius@gmail.com>
2023-02-20 09:57:51 +0100

    Update sketch.js

--- Therapeutic_henley_2023_02_20_08_24_39/sketch.js
+++ Therapeutic_henley_2023_02_20_08_24_39/sketch.js
@@ -1,23 +1,30 @@
 function draw() {
   background(220);
-    image(img, 0, 0);
+  imageMode(CENTER);
+    image(img, windowWidth/2, windowHeight/2.5);
 }
 let img;
 function preload() {
   img = loadImage('Nokia.png');
+
 }
 
 let button;
+let b2;
 function setup() {
   createCanvas(windowWidth, windowHeight);
   background(0);
   button = createButton('Restaffald');
   button.position(width/2, height/2);
   button.mousePressed(changeBG);
+
+  b2 = createButton('Restaffald');
+  b2.position(width/2, height/2.5);
+  b2.mousePressed(changeBG);
 }
 
 function changeBG() {
   let val = random(255);
   background(val);
 }
-Hey 
+
