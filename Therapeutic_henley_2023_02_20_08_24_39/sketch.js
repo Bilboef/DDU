e2d52a549c378f4bf4aacd07a43a5b74a849d993
Bilboef <cd.antonius@gmail.com>
2023-02-20 10:49:25 +0100

    Update sketch.js

--- Therapeutic_henley_2023_02_20_08_24_39/sketch.js
+++ Therapeutic_henley_2023_02_20_08_24_39/sketch.js
@@ -1,10 +1,19 @@
+let visbilleder = true;
+let visbillede2 = false;
+
 function draw() {
   background(220);
   imageMode(CENTER);
   img2.resize(windowWidth, windowHeight);
+  if(visbilleder){
   image(img2, windowWidth/2, windowHeight/2);
 
     image(img, windowWidth/2, windowHeight/5);
+  }
+  if(visbillede2){
+    image(img2, windowWidth/2, windowHeight/2);
+
+  }
 }
 let img;
 let img2;
@@ -25,10 +34,23 @@ function setup() {
 
   b2 = createButton('Bioaffald');
   b2.position(width/2, height/2.5);
-  b2.mousePressed(changeBG);
+  b2.mousePressed(changeBG2);
 }
 
 function changeBG() {
+  b2.hide();
+  button.hide();
+  visbilleder = false;
+  visbillede2 = true;
+  let val = random(255);
+  background(val);
+}
+
+function changeBG2() {
+  b2.hide();
+  button.hide();
+  visbilleder = false;
+  visbillede2 = true;
   let val = random(255);
   background(val);
 }
